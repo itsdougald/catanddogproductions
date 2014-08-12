ca06da411acc456ad28fc00a93d7846d69fcce29
itsdouglad <itsdouglad@users.noreply.github.com>
2014-08-12 22:11:11 +1000

    Update uk.trip.2014.php

--- travel/holidays/uk.trip.2014.php
+++ travel/holidays/uk.trip.2014.php
@@ -18,26 +18,48 @@
 	<h2>UK 2014</h2>
 	With a quick visit back to the UK for Duck's 70th Birthday we had 13 days. We had a big welcome party at Heathrow airport on
 	Saturday 26th @ 6am! With a big greeting and a coffee we head off to Billericay for a night. 
-	<br>
+	</p>
+	<p>
 	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/1.Essex/index.html">First stop Essex</a> [Jul 26th 2014]
-	</br>
+	</p>
 	<p>
 	On Sunday 27th, we drove down to Okehampton in Devon and stayed in the Manor House for 5 nights with Pops, Duck, brothers and
 	their family!
-	<br>
+	</p>
+	<p>
 	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/2.Manor House/index.html">Manor House</a> [Jul 27th - Aug 1st 2014]      
-	</br>
+	</p>
 	<p>
 	Driving into London, Hampton Court, dropped of V and the kids with John/Anna and headed back to Billericay to drop the car
 	off and spend a night/day with Pops and Duck
 	</p>	
 	<p>
 	Sunday 3rd, John and Anna opened their house up to host a BBQ - friends came and went for the day as we drunk and ate under the scorching
-	sun!
-	<br>
-	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/3.Hampton Court/index.html">Hampton Court</a> [ Aug 2nd - 4th 2014]      
-	</br>
+	sun! Cat and a few girls hit the Prince Of Wale sin Hampton Court!!
 	</p>
+	<p>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/3.Hampton Court/index.html">Hampton Court</a> [ Aug 2nd - 4th 2014]      
+	</p>
+	<p>
+	Monday 4th, Dog played golf with Ginge at <a href="http://www.burhillgolf-club.co.uk/">Burhill Golf club</a>; Jez, who had the runs, joined us for a drink at 
+	the clubhouse. Later that night dog hit Kingston town for a few bevvies with Johnny! In the afternoon, Cat headed south to Pagham with Kirstin and the kids and 
+	joined XCYS and Barometer at the Pagham Caravan site!
+	</p>
+	<p>
+	Tuesday 5th, Anna, Johnny and Dog headed down to Pagham for some Caravan carniage! WIth an afternoon BBQ on the go, we basked in the sun
+	and enjoyed the kids entertainment. Hitting the Games arcade and soaked in the camp's night entertainment!
+	</p>
+	<p>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/4.Pagham/index.html">Pagham</a> [ Aug 4th - 6th 2014]      
+	</p>
+	<p>
+	Wednesday 6th we left camp and headed back to Fareham for our last few days. Thursday 7th we hit Peppa Pig World in Poulton! Oink Oink!!
+	Ending the night with Chinese takeaway, Abbot Ale, Cloudy Rum and whisky - heaven!!</p>
+	<p>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/5.Fareham/index.html">Fareham</a> [ Aug 4th - 6th 2014]      
+	</p>
+	<p>
+	Friday 8th, Dog popped into Gun Wharf with XCYS and topped up his dicounted Paul Smith collection, before heading off to the airport and heading back to Aus!
 	<hr class="hide">
       </div>
       <!-- closes #mainContent-->
