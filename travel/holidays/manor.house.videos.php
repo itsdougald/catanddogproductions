80c94a439368d35f5a0c5b986ac49a733aeae407
itsdouglad <itsdouglad@users.noreply.github.com>
2014-08-12 23:55:29 +1000

    Create manor.house.videos.php

---
+++ travel/holidays/manor.house.videos.php
@@ -0,0 +1,38 @@
+<h3>Manor House madness!</h3>
+<p>
+Lawn activities:
+</p>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/Qilu5ybXa9k" frameborder="0" allowfullscreen></iframe>
+</p>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/pCJ7NAVSXgo" frameborder="0" allowfullscreen></iframe>
+</p>
+Activities
+<iframe width="560" height="315" src="//www.youtube.com/embed/HJHFuQ1YOJs" frameborder="0" allowfullscreen></iframe>
+<p>
+Fun house:
+</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/65akLVdGQJA" frameborder="0" allowfullscreen></iframe>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/fz8QtpV66qY" frameborder="0" allowfullscreen></iframe>
+</p>
+<p>
+Let it go!
+</p>
+Which version is better?
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/6OqEtVQbPmI" frameborder="0" allowfullscreen></iframe>
+</p>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/wQpGr13kRTY" frameborder="0" allowfullscreen></iframe>
+</p>
+<p>
+Tennis 
+</p>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/e3eAYyUQEGo" frameborder="0" allowfullscreen></iframe>
+</p>
+<p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/KuQpgWYZDlk" frameborder="0" allowfullscreen></iframe>
+</p>
