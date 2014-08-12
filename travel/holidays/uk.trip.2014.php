de8837ebe2936ed25a9a786ea178e535bfc9b5a0
itsdouglad <itsdouglad@users.noreply.github.com>
2014-08-12 21:28:30 +1000

    Update uk.trip.2014.php

--- travel/holidays/uk.trip.2014.php
+++ travel/holidays/uk.trip.2014.php
@@ -16,10 +16,28 @@
 
 	<div id="mainContent">
 	<h2>UK 2014</h2>
-	<ul>
-		<li><a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/1.Essex/index.html">First stop Essex</a> [Jul 2014]</li>      
-	</ul>
-	
+	With a quick visit back to the UK for Duck's 70th Birthday we had 13 days. We had a big welcome party at Heathrow airport on
+	Saturday 26th @ 6am! With a big greeting and a coffee we head off to Billericay for a night. 
+	<br>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/1.Essex/index.html">First stop Essex</a> [Jul 26th 2014]
+	</br>
+	<p>
+	On Sunday 27th, we drove down to Okehampton in Devon and stayed in the Manor House for 5 nights with Pops, Duck, brothers and
+	their family!
+	<br>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/2.Manor House/index.html">Manor House</a> [Jul 27th - Aug 1st 2014]      
+	</br>
+	<p>
+	Driving into London, Hampton Court, dropped of V and the kids with John/Anna and headed back to Billericay to drop the car
+	off and spend a night/day with Pops and Duck
+	</p>	
+	<p>
+	Sunday 3rd, John and Anna opened their house up to host a BBQ - friends came and went for the day as we drunk and ate under the scorching
+	sun!
+	<br>
+	<a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/3.Hampton Court/index.html">Hampton Court</a> [ Aug 2nd - 4th 2014]      
+	</br>
+	</p>
 	<hr class="hide">
       </div>
       <!-- closes #mainContent-->
