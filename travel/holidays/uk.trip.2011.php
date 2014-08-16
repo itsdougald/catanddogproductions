6b7d1bdccd6454fe5a682635513697b277c24871
itsdouglad <itsdouglad@users.noreply.github.com>
2014-08-16 14:30:22 +1000

    Update uk.trip.2011.php

--- travel/holidays/uk.trip.2011.php
+++ travel/holidays/uk.trip.2011.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Created on Sep 2007 by Neil Guppy aka Dougal ask The Dog
+ * Created on Aug 2014 by Neil Guppy aka Dougal ask The Dog
  *
  * I am the homepage to the cat and dog dot dom productions
  */
@@ -16,16 +16,23 @@
 
 	<div id="mainContent">
 	<h2>UK 2011</h2>
-	<p>
-	
+	With a quick visit back to the UK for Sophia's Birthday we had 19 days. The Smurfs gave us a big welcome and we headed down
+	somewhere.....
 	</p>
+	<p>
+	<strong>Pictures:</strong> <a href="<?php echo $EXETEL_HOME_2014 ?>/07Jul/Uk.trip/1.Essex/index.html">First stop Essex</a> 
+	</p>
+	<p>
+	<iframe width="560" height="315" src="//www.youtube.com/embed/9iGBmpEGDXQ" frameborder="0" allowfullscreen></iframe></p>
+	<p>
+	 heading back to Aus with Cat, Sophia and Andre! 
 	<hr class="hide">
       </div>
       <!-- closes #mainContent-->
     </div>
     <!-- closes #mBody-->
   </div>
-    
+  <!-- closes #container -->
    
-  <!-- footer  -->
-  <? include $ROOT.'/includes/footer.php'; ?>
+  <!-- footer counter -->
+  <?php include $ROOT.'/includes/footer.php'; ?>
