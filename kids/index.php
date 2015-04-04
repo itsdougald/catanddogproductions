b5ae082eaa61c18a2cee7d71979f4f3bad6a44dc
itsdouglad <itsdouglad@users.noreply.github.com>
2015-04-04 22:10:32 +1100

    Update index.php

--- kids/index.php
+++ kids/index.php
@@ -22,8 +22,33 @@
 	<div id="mainContent">
 	<p>
 <strong>Kids!</strong><br>
-<a href="/kids/sophia.php">Sophia</a><br/>
-<a href="/kids/andre.php">Andre</a><br/>
+<ul>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/01Jan/kids/index.html">Jan</a></li>
+</ul>
+<h3>2011</h3>
+<ul>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/12Dec/kids/index.html">Dec</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/11Nov/kids/index.html">Nov</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/10Oct/kids/index.html">Oct</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/09Sep/kids/index.html">Sep</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/08Aug/kids/index.html">Aug</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/07Jul/kids/index.html">July</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/06Jun/kids/index.html">June</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/05May/kids/index.html">May</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/04Apr/kids/index.html">Apr</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/03Mar/kids/index.html">Mar</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/02Feb/kids/index.html">Feb</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2011 ?>/01Jan/kids/index.html">Jan</a></li>
+</ul>
+<h3>2010</h3>
+<ul>
+<li><a href="<?php echo $EXETEL_HOME_2010 ?>/12Dec/kids/index.html">Dec</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2010 ?>/11Nov/kids/index.html">Nov</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2010 ?>/10Oct/kids/index.html">Oct</a></li>
+<li><a href="<?php echo $EXETEL_HOME_2010 ?>/09Sep/kids/index.html">Sep</a></li>
+ <li><a href="<?php echo $EXETEL_HOME_2010 ?>/08Aug/kids/index.html">Aug</a></li>
+    <li><a href="<?php echo $EXETEL_HOME_2010 ?>/07Jul/kids/index.html">July</a></li>
+  </ul>
 
 
         <hr class="hide">
