6149b49dffe751ee3f1eac825241623682785012
itsdougald <itsdougald@users.noreply.github.com>
2015-07-27 21:00:18 +1000

    Update 2014.php

--- australia/2014.php
+++ australia/2014.php
@@ -47,11 +47,19 @@
 
 	<h3>Sport</h3>
 	<ul>
-	
+	<li>SEMI-FINAL-2: Coogee United 3 (0) v (0) 0 Queens Park Lions [ 2014 Aug 31 ]</li>
+	<li>SEMI-FINAL-1: UNSW 1 (0) v (2) 4 Queens Park Lions [ 2014 Aug 24 ]</li>
 	<li>Queens Park Lions 1 (1) v (0) 1 Coogee United [ 2014 Aug 10 ]</li>
-	<li>Queens Park Lions 1 (0) v (0) 0 Maroubra United [ 2014 Aug 3 ]</li>
+	<li>Maroubra United  0 (0) v (0) 1 Queens Park Lions [ 2014 Aug 3 ]</li>
 	<li>Queens Park Lions 0 (0) v (0) 2 Dunbar Rovers [ 2014 Jul 27 ]</li>
-	<li>Maroubra United (0) v (4) 9 Queens Park Lions [ 2014 Jul  ]</li>
+	<li>Maroubra United 0 (0) v (1) 2 Queens Park Lions [ 2014 Jul 22 ]</li>
+	<li>Queens PArk Lions 2 (2) v (2) 4 UNSW [ 2014 Jul 20 ]</li>
+	<li>Waverley Old Boys 0 (0) v (0) 2 Queens Park Lions [ 2014 Jul 13 ]</li>
+	<li>Waverley Old Boys 1 (0) v (0) 1 Queens Park Lions [ 2014 Jul 11 ]</li>
+	<li>Queens Park Lions 1 (0) v (0) 0 Coogee United [ 2014 Jul 6 ]</li>
+	<li>Queens PArk Lions 2 (1) v (0) 4 Dunbar Rovers [ 2014 Jul 5 ]</li>
+	<li>UNSW 2 (1) v (2) 4 Queens Park Lions [ 2014 Jun 30 ]</li>
+	<li>Queens Park Lions 1 (1) v (1) 2 Coogee United [ 2014 May 25]</li>
 	<li>Maroubra United 2 (1) v (0) 1 Queens Park Lions [ 2014 May 11]</li>
 	<li>Queens Park Lions 4 (3) v (1) 4 UNSW [ 2014 Apr 13 ]</li>
 	<li>Waverley Old Boys 1 (0) v (3) 2 Queens Park Lions [ 2014 Apr 6 ]</li>
