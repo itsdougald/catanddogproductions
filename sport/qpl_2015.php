6e8d9b8a0acee9221c19c10a5e586c027a0f8921
itsdougald <itsdougald@users.noreply.github.com>
2015-08-01 12:14:54 +1000

    Create qpl_2015.php

---
+++ sport/qpl_2015.php
@@ -0,0 +1,178 @@
+<?php
+/*
+ * @Author The Dog 
+ * @Created 1 Aug 2015
+ * 
+ * I am the page with match reports from Queens Park games!
+ */
+?>
+<?php 
+ 	include '../application.php'; 
+	include $ROOT.'/includes/header.php'; 
+?>	
+<body id="catanddog" class="homepage">
+<?php
+	include $ROOT.'/includes/tab.php'; 
+ 	include $ROOT.'/includes/side.php'; ?>
+
+	<div id="mainContent">
+
+
+        <h2>Queens Park Lions' match Reports 2015</h2>
+        
+		<h4><a name="match-13"></a> [ 2015 Jul 26]</h4>
+		G'day,
+
+Who is that team in the black and red but not white...mighty mighty Queens Park!!
+
+I am so glad I have nothing to do with Coogee United.
+
+Every time we play the "Clams" it makes me feel so proud to be a Porky. They hack, swear and play crap football. I mean, they really do. They're also not shy in turning on each other loudly on the field. Disgraceful.
+
+I just had to get that off my chest now. Right now.
+
+So 3pm, QP on pitch (whatever the number is - it changes every year like every different swear word out of a Coogee Clam mouth!).
+
+Summer is knocking on the door and boy it's exciting. 
+
+The air was warm but the wind was strong.
+
+With 13 very eager Porkies, had our keeper sorted out from the start, with "Hipster" Lydia kindly putting her Porky hoof up for the whole game - thank you replied the other 12 Porksters.
+
+Bizzarely we didn't have a ref but with minutes to go, had two. One who offered had the wit of a clown. AS we know clowns are normally silent  who normally wouldn't talk - I think we all agreed he should have kept his mouth closed. Awkward! Luckily the allocated ref pushed him aside and took control. Phew.
+
+What a game.
+
+Conditions made it tough for both sides. 
+
+The Porkies were so hot and quick out of the blocks we scored. Yep. 1 nil to the black..... and red not white!!! WTF. OMG. The Clams were stunned but the QP support weren't!! 
+
+We passed and moved. Moved and passed. Coogee didn't know what to do. 
+
+Until 25 minutes in the Clams woke up. With the sun and wind in Lydia's eyes popped in an equaliser through Lydia's trusty hands. 1-1. D'oh.
+
+With an even battle thereon, we had chances but so did the Clams. A crunching tackle from Birthday girl "saving Grace" was outstanding. Penalty I hear the skanky Clams...no whistle but a big chuckle from me on the sidelines. A little bit of luck on our side? Perhaps. Perhaps not.
+
+We pushed through the middle and had many chances on target but resident Clam keeper kept us at bay.
+
+Half-time whistle blew and we re-grouped. Saving Grace had to dash and Jen had befriended the public toilet (Luke didn't want to use the toilet), eek!! Down to 11 for the start, Porkies had a side cross-wind behind them and hungry for revenge....Porkies revenge!
+
+We had chances and so did they.
+
+With a middle patch of scrappy play Coogee turned it on but never threatened until 25 minutes in. They scored a second. 2-1. Devo. Stunned. Sophia was still asleep, Andre was still kicking our spare match ball away from the half-way line, oh and weeing up against posts. He loves weeing. 2-1 to the Clams.
+
+The Porkies switched on a few minutes later and found composure. Jen had hobbled out from her second home and was up for it! With a handful of corners awarded to us, The Porkies were pressing. Hungry. Our snouts were getting a little cold as the sun dropped but we couldn't leave the Clams ahead.
+
+and then....
+
+....well
+
+...surely not?
+
+Yep.
+
+We equalised.
+
+I say we, it was Alma, the fastest Barista in the West(ern world). 
+
+Goal of the season. Incredibly sweet hit. 40 or so yards out on the left. It strangely reminded me of a goal which an Irish Dunbar player scored against us to win the Championship league. 
+
+This time it was us. 
+
+Boom. Accurately curled into the roof of the net over the capable keepers Clammy hands. 2-2. Yep. Sophia was almost awake. Andre was still weeing and Luke/Liam had been taken home cause they'd run out of clothes.
+
+OMG. 5 minutes to go and we'd equalised. All season teams had scored last minute winners...and  Coogee had equalised in the last two games but this time it was us. Yep. Us. Us. Yep. Us. Yep. Us.
+
+Magical.
+
+UNSW have nipped back into fourth after beating the Bra. With three games left, we need to take (I think) at least 4 points to take fourth spot.
+
+It's tight.
+
+It's exciting.
+
+It's on.
+
+Until next time my loyal globe trotters.
+
+Laters,
+Dougal / Couch / Trigg. :)
+
+<h4><a name="match-12"></a> [ 2015 Jul 19]</h4>
+Hi All,
+
+Sorry for not getting the match report out last night, the excitement during the game and watching Dunbar v UNSW after our game just took its toll. I was asleep by 8pm on the "couch".
+
+"Excitement?" I hear you say near, far and wide....
+
+Well settle in Porkies and I think I might just tell you.....
+
+David Phillips the venue. 1pm KO. The sun was shining and 11 (7 away!!) lean mean grilling Porky machines were ready for battle!
+
+The ref didn't show but a bearded hipster did and what a hipster he was. He thanked me at the beginning of the game and after. Admittedly I presented him with a cold Coopers Green at the end but he seriously deserved it. He da man!!
+
+UNSW had beaten second place Coogee 1 nil last week and taken our fourth spot. We had to win. We needed three points. Our throats were dry and nerves were kicking in...why? Cause we care!!!!!
+
+The stadium was poised. The kids were pumped (not asleep at lunchtime?) and the bearded hipster ref got the game under way. 
+
+UNSW came out with a bang. Luckily not hitting the back of the net, got two corners in the space of 5 minutes. We couldn't get out of our half. Was it going to be one of those days? None of us knew.
+
+7 and half minutes on my new touch screen watch ($9.99 from San Sebastian!), our throats had cleared and the day changed for the Red & Black for the rest of the day.
+
+We started creating real chances....a shot over the bar and a few at the keeper Brooke opened the account from a cross. BANG! 1 nil. UNSW were stunned. The hipster ref was impressed. 
+
+5 minutes later. Bang. Lisa scores from the left. 2 nil. What? The Porkies were quietly motoring. Like a Lexus in cruise control. 
+UNSW were being left for dust. With an abundance of 18 year old students being pulled on and off, UNSW were struggling to break our play. Sophia and Andre were sensing they were about to get my undivided attention on the sidelines. Yes, I was beginning to relax and listening to the chants of "I'm hungry" from gup-a and gup-b.
+
+And booooooooom! The hipster ref awarded us a free kick on the edge of the area. Alma stepped up and most of the Porkies walked away before it was taken (?)....I think for some reason we presumed Alma was going to score. The Porkies guessed right. Boom!! Curled over the wall from the right and into the left top corner. All three goals were impressive and unique. The hipster blew for half time and the Porkies were 3 nil up.
+
+Were there more goals to come? But from which team?.... 
+
+With a few "individual" tips from me, the Porkies trotted out from the shade and into the sun. 
+
+The afternoon turned out to be simply "magical", as Sophia likes to put it.
+
+Welsh troll, Dunbar 1st team coach turned up with his team and brought along Dunbar second team coach (Who's also Welsh, good gracious). Sadly I had to talk to him. I congratulated him on winning the State Cup but didn't fail to mention Brookvale hadn't entered - the very team we lost to in the semi-finals last year. 
+
+"You're 3 nil up today", he sings. 
+"you're battling for fourth spot with UNSW I see", he sings again. 
+
+His detailed knowledge our team is worryingly accurate. He stalked us in Div 1 and now in Div 2. Eek. Thankfully my son was peeing on the hockey pitch next door (yep, there was a hockey match going on) so I luckily escaped before he rattled off more personal stats of my old and new team. Is he really on the ESFA board?
+
+Once Andre's wet pants and trousers were off, Alma opened the account for the second half (very considerate I din;t miss the goal!). 4 nil. Or 1 nil second half. This was truly fantastic. The students were baffled. 
+
+But our scoring didn't stop there. Not only did we score from a corner taken by Alma but Tess' header into the back of the net was AMAZEBALLS!!!!!! The net rippled with power. Andre's trousers were still off, Sophia in stitches with Leaping Luke-walker and Liam-wanting-to-leap!!!! 5 nil. What on earth?
+
+The Dunbar trolls were startled. 
+
+Our attacking midfielders and forwards pressed with many more goal scoring opportunities. Seriously. Lydia rattling the cross bar and bouncing over the line (where's our goal-line technology?!) had a few more chances but finally got a penalty awarded for a wayward tackle in the area. Stepping up, slotted in the 6th and final goal for the Porkies!
+
+Every goal was different and every goal was "magical". 6 nil. Outstanding.
+
+Once our song resonated around David Phillips, the Porkies settled down with a case of beer (supplied by UNSW' economics lecturer) on the sidelines and watched Dunbar take on UNSW. Who did we support I hear you all ask? hehe. 
+
+t'was a great game. UNSW scored a quick opener! Dunbar pressed with a very quick forward making most of the chances. Dunbar equalised. Can't remember if Dunbar scored before half-time but myself and kids provided some entertainment on the pitch. Sophia lying down. Andre doing handstands and Luke refusing to leave the field. Grace then followed up with a kick of the match ball at a Dunbar defenders head. Wow. If looks could kill. 
+
+Anyway I continued to eat salt & vinegar crisps off David Phillips' astroturf (I love the turf that much!), more beers were sunk, the kids provided more entertainment and what made the Porkies day and got the loudest roar, was when UNSW scored and equalised in the dying minutes!!!! 
+
+The Welsh troll(s) had been silenced and the Red & Black had an extra curl in their tails!
+
+A day we'll not forget. 
+
+See you all Tuesday night for training Porkies!
+Laters,
+Dougal / Couch / Trigg. :)
+PS Here's Alama's free-kick I "just" caught on my phone.
+<iframe width="425" height="349" src="https://www.youtube.com/watch?v=XoYMOmGtaaI" frameborder="0" allowfullscreen></iframe>
+
+		
+	   <hr class="hide">
+      </div>
+      <!-- closes #mainContent-->
+    </div>
+    <!-- closes #mBody-->
+  </div>
+ 
+   
+  <!-- footer  -->
+  <?php include $ROOT.'/includes/footer.php'; ?>
