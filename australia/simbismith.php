8673d10646de367519a910603cc277b3df64dcd2
itsdougald <itsdougald@users.noreply.github.com>
2015-11-15 14:36:19 +1100

    Update simbismith.php

--- australia/simbismith.php
+++ australia/simbismith.php
@@ -25,7 +25,11 @@
 All pictures are here:<br>
 <a href="<?php echo $EXETEL_HOME_2015 ?>/11Nov/simbismith/index.html">Simbi-smith</a>
 </p>
+Coogee<br>
+Gold Coast<br>
+
 <strong>Day 1: check-in, beach, pool, tennis</strong><br>
+<iframe width="420" height="315" src="https://www.youtube.com/embed/8XRKe--JXYM" frameborder="0" allowfullscreen></iframe>
 <strong>Day 2: boat trip</strong><br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/sWO_2sXse5E" frameborder="0" allowfullscreen></iframe><br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/y7dmU2qualw" frameborder="0" allowfullscreen></iframe><br>
@@ -35,6 +39,9 @@ All pictures are here:<br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/H8Du0WMiIJU" frameborder="0" allowfullscreen></iframe>
 <strong>Day 5: Last day: beach</strong>
 <p>
+Coogee<br>
+Andre's Birthday<br>
+table-tennis<br>
 
 		<hr class="hide">
       </div>
