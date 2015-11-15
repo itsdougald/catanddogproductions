366063ff83b1918bd0c489a07118e68a2b673f58
itsdougald <itsdougald@users.noreply.github.com>
2015-11-15 14:05:51 +1100

    Update simbismith.php

--- australia/simbismith.php
+++ australia/simbismith.php
@@ -4,7 +4,7 @@
  * @date 15 Nov 2015
  *  			
  *
- * I am the page describing simbi-smith holiday
+ * I am the page describing simbi-smith holiday in 2015
  */
 ?>
 <?php 
@@ -25,53 +25,16 @@
 All pictures are here:<br>
 <a href="<?php echo $EXETEL_HOME_2015 ?>/11Nov/simbismith/index.html">Simbi-smith</a>
 </p>
-<strong>Diary:</strong><br>
-<strong>Eve:</strong> Brazillain Christmas Dinner<br>
-<strong>Day:</strong> Party at house with pool in Bronte (visit 1) and a few little videos from the day (uploading more but taking a little time to upload so will email the links tomorrow!):
-
+<strong>Day 1: check-in, beach, pool, tennis</strong><br>
+<strong>Day 2: boat trip</strong><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/sWO_2sXse5E" frameborder="0" allowfullscreen></iframe><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/y7dmU2qualw" frameborder="0" allowfullscreen></iframe><br>
+<strong>Day 3: tennis, pelican feeding</strong><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/piUNR7GKL60" frameborder="0" allowfullscreen></iframe><br>
+<strong>Day 4: Sea World for mums and kids and Melbournce Cup for the male adults!</strong><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/H8Du0WMiIJU" frameborder="0" allowfullscreen></iframe>
+<strong>Day 5: Last day: beach</strong>
 <p>
-        Christmas morning <br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/P0Ul8olGoNk" frameborder="0" allowfullscreen></iframe>
-          </p>
-<p>
-         Main pressie for Sophia <br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/rqlHrNg7UEE" frameborder="0" allowfullscreen></iframe>
-          </p>
-<p>
-         Playing with main pressie <br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/QUTSr-KWu0k" frameborder="0" allowfullscreen></iframe>
-          </p>
-<p>
-         Bronte house party: first dance!  <br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/XFKKnXyiAhU" frameborder="0" allowfullscreen></iframe>
-          </p>
-<p>
-          Bronte house party: Just Dance Cat & bubs!<br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/ExJQ_0_cdl0" frameborder="0" allowfullscreen></iframe>
-          </p>
-<p>
-          Bronte house party: Just Dance Dog & Santa!<br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/ej1Ic8UO1yg" frameborder="0" allowfullscreen></iframe>
-          </p>
-
-
-<strong>Boxing Day:</strong> Party at house with pool in Bronte (visit 2) with nurses/partners<br>
-<strong>27th:</strong> Guppy day (need to upload photos) with nurses/partners<br>
-<strong>31st:</strong> Sat in Dudley Reserve (Dover Heights) to watch Sydney fireworks - a few football girls were there too!<br>
-
-and two video clips from New Year:
-
-<p>
-          Build up:<br>
-        <iframe width="425" height="349" src="http://www.youtube.com/embed/re-u--5QC2o" frameborder="0" allowfullscreen></iframe>
-          </p>
-
-Midnight fireworks! Cant hear them but a local DJ mixes a few tunes to set the atmosphere, oh and a little commentary from me! I tried to get Sophia, who was on my shoulders (she had a power nap after the 9pm fireworks to be awake for the midnight ones!) - you can just about see her face - she was in awe and hasn't stopped talking about them since!!!!!!
-
-<p>
-          Fireworks<br>
-        <iframe width="560" height="315" src="http://www.youtube.com/embed/94FD8b49hAY" frameborder="0" allowfullscreen></iframe>
-          </p>
 
 		<hr class="hide">
       </div>
