047e1a71818b28cae51bcd0d23a0f773ab8cb596
itsdougald <itsdougald@users.noreply.github.com>
2016-04-18 15:40:26 +1000

    Create 2016.php

---
+++ australia/2016.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * @author The Dog
+ * @date 10 Jan 2015
+ *  *
+ * I am the page describing what we get up to in Australia whilst we live the dream in 2015
+ * 
+ * */
+?>
+
+<?php 
+ 	include '../application.php'; 
+	include $ROOT.'/includes/header.php'; 
+?>
+	
+<body id="catanddog" class="homepage">
+<?php
+	include $ROOT.'/includes/tab.php'; 
+ 	include $ROOT.'/includes/side.php'; ?>
+
+	<div id="mainContent">
+	
+		<h1>Australia: Living the dream in 2014</h1>
+		<h3>General 2014</h3>
+	<ul>
+  
+	</ul>
+
+	<h3>Sport</h3>
+	<ul>
+	<li>SEMI-FINAL-2: Coogee United 3 (0) v (0) 0 Queens Park Lions [ 2014 Aug 31 ]</li>
+	<li>SEMI-FINAL-1: UNSW 1 (0) v (2) 4 Queens Park Lions [ 2014 Aug 24 ]</li>
+	<li>Queens Park Lions 1 (1) v (0) 1 Coogee United [ 2014 Aug 10 ]</li>
+	<li>Maroubra United  0 (0) v (0) 1 Queens Park Lions [ 2014 Aug 3 ]</li>
+	<li>Queens Park Lions 0 (0) v (0) 2 Dunbar Rovers [ 2014 Jul 27 ]</li>
+	<li>Maroubra United 0 (0) v (1) 2 Queens Park Lions [ 2014 Jul 22 ]</li>
+	<li>Queens PArk Lions 2 (2) v (2) 4 UNSW [ 2014 Jul 20 ]</li>
+	<li>Waverley Old Boys 0 (0) v (0) 2 Queens Park Lions [ 2014 Jul 13 ]</li>
+	<li>Waverley Old Boys 1 (0) v (0) 1 Queens Park Lions [ 2014 Jul 11 ]</li>
+	<li>Queens Park Lions 1 (0) v (0) 0 Coogee United [ 2014 Jul 6 ]</li>
+	<li>Queens PArk Lions 2 (1) v (0) 4 Dunbar Rovers [ 2014 Jul 5 ]</li>
+	<li>UNSW 2 (1) v (2) 4 Queens Park Lions [ 2014 Jun 30 ]</li>
+	<li>Queens Park Lions 1 (1) v (1) 2 Coogee United [ 2014 May 25]</li>
+	<li>Maroubra United 2 (1) v (0) 1 Queens Park Lions [ 2014 May 11]</li>
+	<li>Queens Park Lions 4 (3) v (1) 4 UNSW [ 2014 Apr 13 ]</li>
+	<li>Waverley Old Boys 1 (0) v (3) 2 Queens Park Lions [ 2014 Apr 6 ]</li>
+	<li>Queens Park Lions 0 (0) v (3) Brookvale [ 2014 Mar 23 ]</li>
+	<li>Brookvale 0 (0) v (2) 4 Queens Park Lions (Emma G 2, Tess, Emma C) [ 2014 Mar 16 ]</li>
+	</ul>
+
+		<h3>Kids</h3>
+	<ul>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/11Nov/kids/index.html">Nov</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/10Oct/kids/index.html">Oct</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/09Sep/kids/index.html">Sep</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/08August/kids/index.html">August</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/07July/kids/index.html">July</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/06June/kids/index.html">June</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/05May/kids/index.html">May</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/04Apr/kids/index.html">Apr</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/03Mar/kids/index.html">Mar</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/02Feb/kids/index.html">Feb</a></li>
+	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/01Jan/kids/index.html">Jan</a></li>
+
+		</ul>
+		<hr class="hide">
+      </div>
+      <!-- closes #mainContent-->
+    </div>
+    <!-- closes #mBody-->
+  </div>
+   	
+  <!-- footer counter -->
+  <?php include $ROOT.'/includes/footer.php'; ?>
