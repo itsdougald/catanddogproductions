b6eeed5983d423953775656757e89dc3a7cbebe6
itsdougald <itsdougald@users.noreply.github.com>
2016-04-18 16:45:58 +1000

    Update 2015.php

--- australia/2015.php
+++ australia/2015.php
@@ -20,49 +20,49 @@
 
 	<div id="mainContent">
 	
-		<h1>Australia: Living the dream in 2014</h1>
-		<h3>General 2014</h3>
-	<ul>
-  
-	</ul>
-
-	<h3>Sport</h3>
-	<ul>
-	<li>SEMI-FINAL-2: Coogee United 3 (0) v (0) 0 Queens Park Lions [ 2014 Aug 31 ]</li>
-	<li>SEMI-FINAL-1: UNSW 1 (0) v (2) 4 Queens Park Lions [ 2014 Aug 24 ]</li>
-	<li>Queens Park Lions 1 (1) v (0) 1 Coogee United [ 2014 Aug 10 ]</li>
-	<li>Maroubra United  0 (0) v (0) 1 Queens Park Lions [ 2014 Aug 3 ]</li>
-	<li>Queens Park Lions 0 (0) v (0) 2 Dunbar Rovers [ 2014 Jul 27 ]</li>
-	<li>Maroubra United 0 (0) v (1) 2 Queens Park Lions [ 2014 Jul 22 ]</li>
-	<li>Queens PArk Lions 2 (2) v (2) 4 UNSW [ 2014 Jul 20 ]</li>
-	<li>Waverley Old Boys 0 (0) v (0) 2 Queens Park Lions [ 2014 Jul 13 ]</li>
-	<li>Waverley Old Boys 1 (0) v (0) 1 Queens Park Lions [ 2014 Jul 11 ]</li>
-	<li>Queens Park Lions 1 (0) v (0) 0 Coogee United [ 2014 Jul 6 ]</li>
-	<li>Queens PArk Lions 2 (1) v (0) 4 Dunbar Rovers [ 2014 Jul 5 ]</li>
-	<li>UNSW 2 (1) v (2) 4 Queens Park Lions [ 2014 Jun 30 ]</li>
-	<li>Queens Park Lions 1 (1) v (1) 2 Coogee United [ 2014 May 25]</li>
-	<li>Maroubra United 2 (1) v (0) 1 Queens Park Lions [ 2014 May 11]</li>
-	<li>Queens Park Lions 4 (3) v (1) 4 UNSW [ 2014 Apr 13 ]</li>
-	<li>Waverley Old Boys 1 (0) v (3) 2 Queens Park Lions [ 2014 Apr 6 ]</li>
-	<li>Queens Park Lions 0 (0) v (3) Brookvale [ 2014 Mar 23 ]</li>
-	<li>Brookvale 0 (0) v (2) 4 Queens Park Lions (Emma G 2, Tess, Emma C) [ 2014 Mar 16 ]</li>
-	</ul>
-
-		<h3>Kids</h3>
-	<ul>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/11Nov/kids/index.html">Nov</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/10Oct/kids/index.html">Oct</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/09Sep/kids/index.html">Sep</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/08August/kids/index.html">August</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/07July/kids/index.html">July</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/06June/kids/index.html">June</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/05May/kids/index.html">May</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/04Apr/kids/index.html">Apr</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/03Mar/kids/index.html">Mar</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/02Feb/kids/index.html">Feb</a></li>
-	<li><a href="<?php echo $EXETEL_HOME_2014 ?>/01Jan/kids/index.html">Jan</a></li>
-
-		</ul>
+		<h1>Australia: Living the dream in 2015</h1>
+		<h3>General</h3>
+  <ul>
+     <li><a href="/">Simbi-Smith's land after 10 years since their last holiday down under!!</a> [ 2015 Oct 28 - Nov 11 ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/10Oct/ffa/index.html">Dog starts a new job at the FFA</a> [ 2015 Oct 26 ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/10Oct/guptoberfest/index.html">Guptoberfest is back!!!!</a></a> [ 2015 Oct ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/10Oct/sophia.nippers/index.html">Sophia at Clovelly Nippers</a> [ 2015 Oct 11 ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/10Oct/dog.time.off/index.html">Dog's time off</a> [ 2015 Oct ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/10Oct/abc.leaving/index.html">Dog has left the ABC</a> [ 2015 Oct 9th ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/09Sep/coogee.food.festival/index.html">Coogee Food Festival</a> [ 2015 Sep ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/07Jul/dog.bday/index.html">Dog Birthday</a> [ 2015 July ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/07Jul/sophia.bday/index.html">Sophia Birthday</a> [ 2015 July ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/06Jun/Sophia.award/index.html">Sophia award</a> [ 2015 June ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/06Jun/Rich.30/index.html">Rich 30th</a> [ 2015 June ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/06Jun/american.bbq/index.html">American BBQ</a> [ 2015 June ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/05May/hunter.valley/index.html">Hunter Valley with the family</a> [ 2015 May ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/04Apr/Easter/index.html">Easter fun</a> [ 2015 Apr ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/01Jan/kirkin/index.html">Kirkin's 40th!</a> [ 2015 Jan ]</li>
+     <li><a href="<?php echo $EXETEL_HOME_2015 ?>/01Jan/SummerHols/index.html">Summer Holiday in Berry</a> [ 2015 Jan ]</li>
+  </ul>
+  <h3>Sport</h3>
+  <ul>
+    <li>Preliminary-final: Dunbar Rovers AAW2 0 (0) v (0) 1 Queens Park AAW2 [ 2015 Aug 23 ]</li>
+    <li>Semi-final: Loko AAW2 2 (0) v (1) 1 Queens Park AAW2 [ 2015 Aug 16 ]</li>
+    <li>Queens Park AAW2 5 (2) v (0) 1 Maroubra United AAW2 [ 2015 Aug 7 ]</li>
+    <li>Loko AAW2 2 (0) v (1) 4 Queens Park AAW2 [ 2015 Jul 31 ]</li>
+    <li>Loko AAW2 2 (1) v (1) 2 Coogee United AAW2 [ 2015 Jul 26 ]</li>
+    <li>Queens Park AAW2 2 (1) v (1) 2 Coogee United AAW2 [ 2015 Jul 26 ]</li>
+    <li>UNSW 0 (0) v (3) 6 Queens Park AAW2 [ 2015 Jul 19 ]</li>
+    <li>Dunbar Rovers 1 (0) v (0) 0 Queens Park AAW2 [ 2015 Jul 12 ]</li>
+    <li>Maroubra United 0 (0) v (4) 7 Queens Park AAW2 [ 2015 Jul 4 ]</li>
+    <li>Queens Park 2 (1) v (1) 3 Dunabr Rovers AAW2 [ 2015 Jun 28 ]</li>
+    <li>Coogee United 1 (0) v (0) 1 Queens Park AAW2 [ 2015 Jun 14 ]</li>
+    <li>Queens Park 3 (0) v (0) 0 UNSW AAW2 [ 2015 Jun 7 ]</li>
+    <li>Queens Park 1 (1) v (1) 4 Dunbar AAW2 [ 2015 May 31 ]</li>
+    <li>Queens Park 4 (3) v (0) 2 Maroubra United AAW2 [ 2015 May 24 ]</li>
+    <li>Queens Park 2 (1) v (2) 3 Loko Cove AAW2 [ 2015 May 17 ]</li>
+    <li>Coogee United 1 (0) v (0) 1 Queens Park AAW2 [ 2015 May 10 ]</li>
+    <li>UNSW 1 (0) v (0) 0 Queens Park AAW2 [ 2015 May 3 ]</li>
+    <li>Queens Park 1 (0) v (0) 0 Maroubra United AAW2 [ 2015 Apr 19 ]</li>
+    <li>Loko Cover 3 (3) v (0) 1 Queens Park AAW2 [ 2015 Apr 12 ]</li>
+    <li>Queens Park AAW2 2 (0) v (0) 2 Coogee United AAW2 [ 2015 Mar 29]</li>
+  </ul>
 		<hr class="hide">
       </div>
       <!-- closes #mainContent-->
